a247fd83c15aba32ce3dc07a1da04707e88b093d
DEVYANSHU GAGNEJA <109970405+devyanshugagneja@users.noreply.github.com>
2024-12-06 22:25:20 +0530

    frontend's ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
+import LandingPage from './Components/LandingPage'
+import LoginPage from './Components/Login'
+import SignupPage from './Components/Signup'
+import OTPVerification from './Components/OtpVerification'
+import Homepage from './Components/HomePage'
+import UserProfile from './Components/UserPRofile'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/otp-verification" element={<OTPVerification />} />
+        <Route path="/homepage" element={<Homepage />} />
+        <Route path="/user-profile" element={<UserProfile />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
